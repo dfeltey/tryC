f01200156d5acf709778378878fa5b9bc98d243a
Dan Feltey <dfeltey@gmail.com>
2012-10-25 00:16:33 -0800

    added alex file for minC

--- Haskell/minC.hs
+++ Haskell/minC.hs
@@ -1,7 +1,27 @@
 data MinCType = I Integer
-		   	   | F Float
-		   	   | D Double
-		       | C Char
-		       | Void
-		       | Pointer MinCType
-		   
+		   	  | F Float
+		   	  | D Double
+		      | C Char
+		      | Void
+		      | Pointer MinCType -- maybe pointers don't go here since all the others hold values
+		    --| Array ??? where should arrays go??
+		      | EMPTY
+		      | FUN MinCType -- return-val args body
+		      deriving (Eq, Show, Read)
+
+
+-- Might need GADTs for the expressions....
+data MinCExpr =  
+
+
+
+data MinCStatement = IF -- Test Body Rest
+				   | ELSE -- Body Rest, basically run these two things in sequence 
+				   | WHILE -- Test body, rest(/continuation?)
+				   | DO -- basically the same as while 
+				   | FOR -- all of these could probably desugar into while
+				   | RETURN MinCExpr-- value, just give back the value ...
+				   | ASSIGN -- variable and value modify an environment and.or store to hold the new assignment
+				   | DECLARE  -- variable, make space for it in the environment and store?
+				   | EXPR MinCExpr
+				   deriving (Eq, Read, Show)
